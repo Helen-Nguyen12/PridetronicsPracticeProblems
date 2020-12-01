59ef43bfa3be524af256ef5fd934797d8742e2cd
Helen-Nguyen12 <helen.nguyen@pridetronics.com>
2020-12-01 00:22:18 -0700

    Part 1 Completed RepeatedBreakRemember

--- RepeatBreakRemember/RepeatedBreakRemember.java
+++ RepeatBreakRemember/RepeatedBreakRemember.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class RepeatedBreakRemember {
     public static void main(String[] args) {
-     //Part 1
-        
+        // Initalize scanner and grab number from terminal
+        Scanner scanner = new Scanner(System.in);
+        // Asks User for a number
+        System.out.println("Insert a Number:");
+        //Grabs number from terminal
+        int count = scanner.nextInt();
+        if (count == -1) {
+            System.out.println("Thanks! Bye!");
+        }
+        //While loop to asks for number until user puts in -1
+        while (count != -1) {
+            if (count > -1) {
+                System.out.println("Insert a Number:");
+                count = scanner.nextInt();
+            } 
+            else if (count < -1) {
+                System.out.println("Insert a Number:");
+                count = scanner.nextInt();
+            }
+            else if (count == -1) {
+                break;
+            }
+        System.out.println("Thanks! Bye!");
+        }
     }
+    // How does one make the answer repeat?
+    /*
+     * for(int count = scanner.nextInt(); count < 10; count ++){ if (count == -1) {
+     * System.out.print("Thanks! Bye!"); break; } if (count != -1) {
+     * System.out.println("Insert a Number:"); continue; } }
+     * 
+     * //For loop to repeat 10 times, counting by 1 /*for(int count = 1; count <=
+     * 10; count ++){ System.out.println(count); }
+     */
 }
